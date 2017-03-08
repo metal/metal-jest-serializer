497daf0304cdaafa1bba19c4446444d19f2241ad
Michael Hadley <michael.hadley@liferay.com>
2017-03-08 10:12:26 -0800

    Source formatting

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -9,11 +9,11 @@ describe('snapshot serializer', () => {
 
   it('should pass if the value is a metal component', () => {
     const val = {
-    __metal_component__: true
+      __metal_component__: true
     };
 
     expect(serializer.test(val)).toBe(true);
-    });
+  });
 
   it('should serialize to a string', () => {
     const element = document.createElement('div');
